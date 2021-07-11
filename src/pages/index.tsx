16ee044ad55d04a1ab1f07f2fc613e9ee9655da5
lvfengjiao <lvlvfengjiao@163.com>
2021-07-11 17:37:58 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import styles from './index.less';
 
-export default () => {
-  return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
-  );
+export default class CComponent extends React.Component {
+  state = {
+    inputText: "",
+  };
+  onButtonClick(char:any) {
+    // 代码写在这里↓↓↓↓↓
+
+    this.setState((state) => {
+      console.log(state);
+      return { inputText: char };
+    });
+
+    // 代码写在这里↑↑↑↑↑
+  }
+  render() {
+    return (
+      <div>
+        <input value={this.state.inputText} />
+        {[
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "0",
+          "+",
+          "-",
+          "x",
+          "/",
+          "=",
+        ].map((char) => (
+          <button onClick={() => this.onButtonClick(char)}>{char}</button>
+        ))}
+      </div>
+    );
+  }
 }
